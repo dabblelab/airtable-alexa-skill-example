a906bff9f981e1757738ba2edce6271f1c4f21c2
Sohini <sohinipattanayak.it@gmail.com>
2020-12-01 01:33:26 +0530

    Files updated

--- lambda/custom/util.js
+++ lambda/custom/util.js
@@ -1,8 +1,8 @@
-//util.js
 const AWS = require('aws-sdk');
 
 const s3SigV4Client = new AWS.S3({
-    signatureVersion: 'v4'
+    signatureVersion: 'v4',
+    region: process.env.S3_PERSISTENCE_REGION
 });
 
 module.exports.getS3PreSignedUrl = function getS3PreSignedUrl(s3ObjectKey) {
@@ -14,6 +14,6 @@ module.exports.getS3PreSignedUrl = function getS3PreSignedUrl(s3ObjectKey) {
         Expires: 60*1 // the Expires is capped for 1 minute
     });
     console.log(`Util.s3PreSignedUrl: ${s3ObjectKey} URL ${s3PreSignedUrl}`);
-    return s3PreSignedUrl.replace(/&/g,'&amp;');
+    return s3PreSignedUrl;
 
 }
